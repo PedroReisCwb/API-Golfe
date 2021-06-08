1bbe41713a913b55c8f5ea8461b2b83113708de2
Pedro Reis <reis.cwb@hotmail.com>
2021-06-08 08:36:19 -0300

    Ajuste na validação do token

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,4 @@ COPY package*.json ./
 
 COPY . .
 
-EXPOSE 3333:3333
+#EXPOSE 3333:3333
--- app/Controllers/Http/AgendamentoUsuariosController.ts
+++ app/Controllers/Http/AgendamentoUsuariosController.ts
@@ -5,11 +5,16 @@ import Database from "@ioc:Adonis/Lucid/Database"
 export default class AgendamentoUsuariosController {
   public async login({ response, params }) {
     // Rota para buscar todas as agendas
-    const associado = await Database.rawQuery(`SELECT AJ.id id_jogador, AJ.nome nome_jogador, SSC.token FROM SOC_SOCIOS_SENHA SSC WITH (nolock)
-        INNER JOIN AGENDAMENTO_GLF_JOGADOR AJ ON AJ.categoria = SSC.CATEGORIA AND AJ.matricula = SSC.MATRICULA
-        WHERE SSC.token = '${params.token}'
-        AND SSC.STATUS <> 'E'
-        AND SSC.GERACAO = 0`)
+    const associado = await Database.rawQuery(`
+      SELECT
+        CASE WHEN AJ.id <> '' THEN AJ.id ELSE 0 END id,
+        CASE WHEN AJ.nome <> '' THEN AJ.nome ELSE '' END nome,
+        CAST(CASE WHEN AJ.id <> '' THEN 1 ELSE 0 END AS BIT) jogador
+      FROM SOC_SOCIOS_SENHA SSC WITH (nolock)
+      LEFT JOIN AGENDAMENTO_GLF_JOGADOR AJ ON AJ.categoria = SSC.CATEGORIA AND AJ.matricula = SSC.MATRICULA
+      WHERE SSC.token = '${params.token}'
+      AND SSC.STATUS <> 'E'
+      AND SSC.GERACAO = 0`)
 
     if (associado[0]) {
       return response.json(associado[0])
